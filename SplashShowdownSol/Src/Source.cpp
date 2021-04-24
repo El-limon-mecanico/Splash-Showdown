f6a87b7321f728df661bd5742a18b4ef91b542d4
Nirv.Ana <anmartan@ucm.es>
2021-04-24 18:20:45 +0200

    Creación del proyecto
    Cambiado de sitio el submódulo e incluido QuackEngine.

---
+++ SplashShowdownSol/Src/Source.cpp
@@ -0,0 +1,20 @@
+#include "QuackEnginePro.h"
+
+
+#if (defined _DEBUG) || !(defined _WIN32)
+int main() {
+#else
+#include <windows.h>
+int WINAPI
+WinMain(HINSTANCE zHInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
+#endif
+
+
+	// AQUI FALTA MANEJO DE ERRORES Y EXCEPCIONES
+	if (QuackEnginePro::Init()) {
+		QuackEnginePro::Instance()->start();
+	}
+
+
+	return 0;
+}
